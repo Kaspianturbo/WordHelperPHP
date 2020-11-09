cfac68b961740312aeb7110edc96f2272c6fe399
Bohdan <braik978@gmail.com>
2020-11-09 19:53:00 +0200

    nothing new

--- admin/index.php
+++ admin/index.php
@@ -6,11 +6,33 @@
     <header>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="..\css\main.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
     </header>
     <body>
-        <form method="POST" action="fillTemplate.php" enctype="multipart/form-data">
-            <input name="filename" type="file" required>
-            <input type="submit" value="завантажити">
-        </form>
+        <div class="container w-25 mt-5">
+            <form method="POST" action="fillTemplate.php" enctype="multipart/form-data">
+                <div class="input-group">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" name="filename" required>
+                        <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                    </div>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit" id="inputGroupFileAddon04">Button</button>
+                    </div>
+                </div>
+            </form>
+            <div class="input-group">
+  <div class="custom-file">
+    <input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
+    <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+  </div>
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Button</button>
+  </div>
+</div>
+        </div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </html>
